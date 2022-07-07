9dc0bbc3a84e9d7f92d2d3883191743a56fef8c0
giomen88 <giorgia9288@virgilio.it>
2022-07-07 17:05:37 +0200

    correzione incremento like

--- js/script.js
+++ js/script.js
@@ -34,10 +34,10 @@
 // creo array
 
 const feed = [
-    { id: 1, name: 'Pinco Pallino', picture: '../img/pinco-pallino.jpg', date: '07/06/2022', text: 'Non contare ogni giorno che passa per ciò che raccogli, ma contalo per ciò che semini.', image: 'https://unsplash.it/300/300?image=523', likes: 75 },
+    { id: 1, name: 'Pinco Pallino', picture: 'img/pinco-pallino.jpg', date: '07/06/2022', text: 'Non contare ogni giorno che passa per ciò che raccogli, ma contalo per ciò che semini.', image: 'https://unsplash.it/300/300?image=523', likes: 75 },
     { id: 2, name: 'Paperon De Paperoni', picture: '', date: '08/06/2022', text: "L'idealismo è la capacità di vedere le persone come potrebbero essere se non fossero come sono.", image: 'https://unsplash.it/300/300?image=415', likes: 96 },
-    { id: 3, name: 'Leonardo Da Vinci', picture: '../img/leonardo-da-vinci.jpg', date: '09/06/2022', text: "Può essere artista solo colui che ha una intuizione dell'infinito.", image: 'https://unsplash.it/300/300?image=287', likes: 142 },
-    { id: 4, name: 'Hernest Hemingway', picture: '../img/ernest-hemingway.jpg', date: '10/06/2022', text: 'Bisognerebbe imparare dal mare a improvvisare e lasciarsi andare e dal cielo a non avere limiti e confini.', image: 'https://unsplash.it/300/300?image=12', likes: 44 },
+    { id: 3, name: 'Leonardo Da Vinci', picture: 'img/leonardo-da-vinci.jpg', date: '09/06/2022', text: "Può essere artista solo colui che ha una intuizione dell'infinito.", image: 'https://unsplash.it/300/300?image=287', likes: 142 },
+    { id: 4, name: 'Hernest Hemingway', picture: 'img/ernest-hemingway.jpg', date: '10/06/2022', text: 'Bisognerebbe imparare dal mare a improvvisare e lasciarsi andare e dal cielo a non avere limiti e confini.', image: 'https://unsplash.it/300/300?image=12', likes: 44 },
 ]
 
 // funzioni
@@ -107,7 +107,7 @@ for (const likeButton of likeButtons) {
 
         likeButton.classList.toggle('like-button--liked');
 
-        const postId = likeButton.dataset.id;
+        const postId = likeButton.dataset.postid;
 
         const likeCounter = document.getElementById(`like-counter-${postId}`);
 
